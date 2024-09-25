8339ac7f1efa8b92e0e66e07f53bd4a02933e169
LAPTOP-74QC269A\Prateek Runwal <prateekrunwal@gmail.com>
2024-09-25 23:40:05 +0530

    sum of  one and two numbers test case added

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -2,6 +2,10 @@ function add(numbers) {
     if (numbers === "") {
       return 0;
     }
+
+    const numArray = numbers.split(',');
+    const sum = numArray.reduce((acc, num) => acc + parseInt(num), 0);
+    return sum;
   }
   
   module.exports = { add };
--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -5,4 +5,13 @@ describe('String Calculator', () => {
   it('should return 0 for an empty string', () => {
     expect(add("")).to.equal(0);
   });
+
+  it('should return the number itself when one number is provided', () => {
+    expect(add("1")).to.equal(1);
+  });
+
+  it('should return the sum of two comma-separated numbers', () => {
+    expect(add("1,5")).to.equal(6);
+  });
+  
 });
